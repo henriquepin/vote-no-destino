f877343a722e9f61ce34ffa0b08f52f323e73b6d
Henrique Pin <pin.henrique03@gmail.com>
2016-12-24 11:54:43 -0200

    user visits home page-[wip]

--- spec/features/user_visits_home_page_spec.rb
+++ spec/features/user_visits_home_page_spec.rb
@@ -9,5 +9,7 @@ feature 'user visits home page' do
     expect(current_path).to eq root_path
     expect(page).to have_content "Bem vindo!"
     expect(page).to have_content "Por favor, escolha seu destino:"
+    expect(page).to have_content place.id[0]
+    expect(page).to have_content place.id[1]
   end
 end
